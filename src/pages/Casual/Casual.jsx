12f2adda66ff1ee48f182d3a4f883fa1df576244
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-07-30 08:14:52 -0400

    Moving on to modal.

--- src/pages/Casual/Casual.jsx
+++ src/pages/Casual/Casual.jsx
@@ -33,18 +33,17 @@ function Casual() {
   }
 
   function handleAnswerSelect(currentLyric, answer) {
-    pushToLocaleStorage(currentLyric)
+    pushToLocaleStorage(currentLyric.lyric)
 
     // modal logic
-    let currentLyricObject = lyrics.filter((l) => l.lyric === currentLyric)
-    if (currentLyricObject[0].rap === answer) {
+    if (currentLyric.rap === answer) {
       setCorrectAnswers((prev) => prev + 1)
       // show modal
-      console.log("You got it right", answer, currentLyricObject[0].rap)
+      console.log("You got it right", answer, currentLyric.rap)
     } else {
       setIncorrectAnswers((prev) => prev + 1)
       // show modal
-      console.log("WRONG", answer, currentLyricObject[0].rap)
+      console.log("WRONG", answer, currentLyric.rap)
     }
 
     // update available lyrics
@@ -55,19 +54,18 @@ function Casual() {
     if (availableLyrics.length === 0) {
       console.log("No more lyrics available")
       setNoAvailableLyrics(true)
-      setCurrentLyric(null)
+      setCurrentLyric({ lyric: "No more lyrics available" })
       // handle NO MORE LYRICS
     } else {
       setCurrentLyric(
         availableLyrics[Math.floor(Math.random() * availableLyrics.length)]
-          .lyric
       )
     }
   }
 
   function handleInitialLoad() {
     if (!localStorage.getItem("usedLyrics")) {
-      setCurrentLyric(lyrics[Math.floor(Math.random() * lyrics.length)].lyric)
+      setCurrentLyric(lyrics[Math.floor(Math.random() * lyrics.length)])
     } else {
       let availableLyrics = lyrics.filter(
         (item) =>
@@ -75,12 +73,10 @@ function Casual() {
       )
       setCurrentLyric(
         availableLyrics[Math.floor(Math.random() * availableLyrics?.length)]
-          ?.lyric
       )
       if (availableLyrics.length === 0) {
-        console.log("No more lyrics available")
         setNoAvailableLyrics(true)
-        setCurrentLyric(null)
+        setCurrentLyric({ lyric: "No more lyrics available" })
         // handle NO MORE LYRICS
       }
     }
@@ -92,40 +88,49 @@ function Casual() {
 
   return (
     <div className="test">
-      <div className="lyric">{currentLyric}</div>
-      <div className="answer-buttons">
-        <button
-          onClick={() => handleAnswerSelect(currentLyric, true)}
-          className="rap btn"
-        >
-          Rap
-        </button>
-        <button
-          onClick={() => handleAnswerSelect(currentLyric, false)}
-          className="crap btn"
-        >
-          Crap
-        </button>
-      </div>
-      <div className="scoreboard">
-        Correct: {correctAnswers} Incorrect: {incorrectAnswers}
-      </div>
-      <ResultModal />
-      {modalIsShown
-        ? createPortal(
-            <ResultModal />,
-            document.querySelector(".modal-container")
-          )
-        : ""}
+      {!noAvailableLyrics ? (
+        <>
+          <div className="lyric">{currentLyric.lyric}</div>
+          <div className="answer-buttons">
+            <button
+              onClick={() => handleAnswerSelect(currentLyric, true)}
+              className="rap btn"
+            >
+              Rap
+            </button>
+            <button
+              onClick={() => handleAnswerSelect(currentLyric, false)}
+              className="crap btn"
+            >
+              Crap
+            </button>
+          </div>
+          <div className="scoreboard">
+            Correct: {correctAnswers} Incorrect: {incorrectAnswers}
+          </div>
+          {/* <ResultModal /> */}
+          {modalIsShown
+            ? createPortal(
+                <ResultModal />,
+                document.querySelector(".modal-container")
+              )
+            : ""}
+        </>
+      ) : (
+        ""
+      )}
 
       {noAvailableLyrics ? (
-        <div>
+        <div className="no-more-lyrics">
           <div>
             There are no more lyrics available at this time. Click here to clear
             memory and start again.{" "}
           </div>
           <div className="button-div">
             <button onClick={() => handleMemoryClear()}>Clear Memory</button>
+          </div>
+          <div className="scoreboard">
+            Correct: {correctAnswers} Incorrect: {incorrectAnswers}
           </div>
         </div>
       ) : (

f3bd2fe34b02ab90687e7e5fef5cd541c4f8e716
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-08-20 19:07:27 -0400

    100vh fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx"
 import "./index.scss"
 import { LyricsProvider } from "./context/LyricsContext.jsx"
 import { AnswerProvider } from "./context/AnswerContext.jsx"
+import Div100vh from "react-div-100vh"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AnswerProvider>
       <LyricsProvider>
-        <App />
+        <Div100vh>
+          <App />
+        </Div100vh>
       </LyricsProvider>
     </AnswerProvider>
   </React.StrictMode>
